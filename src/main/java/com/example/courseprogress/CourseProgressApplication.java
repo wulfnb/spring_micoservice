ddddfe7b03b59a4c9130ce4f0484eb86c65f62ad
NOUFAL BABU A S <wulfnb@gmail.com>
2025-10-29 10:39:29 +0100

    added constructor

--- src/main/java/com/example/courseprogress/CourseProgressApplication.java
+++ src/main/java/com/example/courseprogress/CourseProgressApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseProgressApplication {
+
+    private CourseProgressApplication() {
+        // Utility class constructor
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(CourseProgressApplication.class, args);
     }
